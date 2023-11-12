535f8fb5e6a50d4ec9b24654110fb67914f29339
N0ah-S <noahalexsimon@rocketmail.com>
2023-11-12 01:28:18 +0100

    Restructuring

---
+++ src/main/java/org/cinemacraftstudios/PlayerStats/Session.java
@@ -0,0 +1,17 @@
+package org.cinemacraftstudios.PlayerStats;
+
+import java.util.Date;
+
+/**
+ * The place where all of the data for each session is stored.
+ * 
+ * @author Simon U.
+ * 
+ */
+public class Session {
+	public Date start;
+	public Date end;
+	public int blocksPlaced = 0;
+	public int worldEditCommandsUsed = 0;
+	public int blocksChangedWorldEdit = 0;
+}
